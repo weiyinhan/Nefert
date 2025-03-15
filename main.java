f9d70ce560a06d5067aa50641fdcdfb2a849dfee
weiyinhan <781203793@qq.com>
2025-03-15 16:44:59 +0800

    代码完成中-1

--- main.java
+++ main.java
@@ -1,1 +1,23 @@
-实现了小功能
+public class Student{
+    private String name;
+    private String studentID;
+    private int score;
+    public Student(String name,String studentID,int score){
+        this.name=name;
+        this.studentID=studentID;
+        this.score=score;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getStudentID(){
+        return studentID;
+    }
+    public int getScore(){
+        return score;
+    }
+}
+import java.util.Scanner;
+public class ScoreQuerySystm{
+    private Student student;
+}
